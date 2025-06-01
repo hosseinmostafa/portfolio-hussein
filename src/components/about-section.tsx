4547e75771b4eafd96f7a813e114de7fc2a6c645
hosseinmostafa <hesseinnmostafaa@gmail.com>
2025-06-01 12:34:44 +0300

    Update name

--- src/components/about-section.tsx
+++ src/components/about-section.tsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                 animate={{ y: 0, opacity: 1 }}
                 transition={{ duration: 0.5, delay: 0.3 }}
               >
-                Alhussein Mostafa
+                Alhussein Mostafa Abdelrahman
               </motion.h1>
 
               <motion.h2
